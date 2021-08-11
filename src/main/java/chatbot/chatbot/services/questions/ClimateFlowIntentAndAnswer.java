4aeb80464dcf62869c91d16db3d94d2af842bb94
ArtDev5 <artur_antoniopw01@hotmail.com>
2021-08-11 10:27:58 -0300

    Entrando na question de fluxo através da flag; Parando de utilizar as intents e utilizando somente as entidades para qualquer tipo de fluxo.

--- src/main/java/chatbot/chatbot/services/questions/ClimateFlowIntentAndAnswer.java
+++ src/main/java/chatbot/chatbot/services/questions/ClimateFlowIntentAndAnswer.java
@@ -23,81 +23,70 @@ public class ClimateFlowIntentAndAnswer implements Question {
 
     @Override
     public boolean verifyIntent(MessageEntity messageEntity) {
-        String intent = messageEntity.getIntent();
-
         String userId = messageEntity.getUserId();
         String flag = messageContext.getContextData(userId, "flag");
-
-        return flag.equals("climate") && intent.equals("ClimateSpecific - Date") ||
-                flag.equals("climate") && intent.equals("ClimateSpecific - Location") ||
-                flag.equals("climate") && intent.equals("ClimateSpecific - DateAndLocation");
+        
+        return flag.equals("climate");
     }
 
     @Override
     public String getAnswer(MessageEntity messageEntity) {
 
         Map<String, String> entities = messageEntity.getEntities();
-        String intent = messageEntity.getIntent();
 
         String userId = messageEntity.getUserId();
-        String answer = "Erro no flow do climate";
+        String answer = "Houve um erro. Consultar suporte.";
 
-        if(intent.equals("ClimateSpecific - DateAndLocation")){
-            String userCity = entities.get("city");
-            String userDate = entities.get("date");
-            String formattedDate = getFormattedDate(userDate);
+        String userCity = entities.get("city");
+        String userDate = entities.get("date");
 
-            messageContext.setContextData(userId, "date", formattedDate);
-            messageContext.setContextData(userId, "city", userCity);
-            answer = "Ok";
-        }
-
-        if(intent.equals("ClimateSpecific - Date")){
-            String date = entities.get("date");
-            String formattedDate = getFormattedDate(date);
-
-            String dateInContext = messageContext.getContextData(userId, "date");
-
-            if(!dateInContext.equals("")){
-                answer = "O dia já foi informado, informe o local.";
-            }else{
-                messageContext.setContextData(userId, "date", formattedDate);
-                answer = "Salvo. Por favor, agora me informe o local.";
-            }
-        }
-
-        if (intent.equals("ClimateSpecific - Location")) {
-            String city = entities.get("city");
-
+        if(userCity != null){
             String cityInContext = messageContext.getContextData(userId, "city");
 
             if(!cityInContext.equals("")){
-                answer = "O local já foi informado, informe o dia.";
+                answer = "Local já foi informado. Informe o dia.";
             }else{
-                messageContext.setContextData(userId, "city", city);
-                answer = "Salvo. Por favor, agora me informe o dia.";
+                answer = "Salvo. Agora informe o dia.";
+                messageContext.setContextData(userId, "city", userCity);
             }
         }
 
-        String verifiedAnswers = verifyAnswers(userId);
+        if(userDate != null){
+            String dateInContext = messageContext.getContextData(userId, "date");
 
-        if (verifiedAnswers.equals("")) {
+            if(!dateInContext.equals("")){
+                answer = "Dia já foi informado. Informe o local";
+            }else{
+                answer = "Salvo. Agora informe o local";
+                String formattedDate = formattedDate = getFormattedDate(userDate);
+                messageContext.setContextData(userId, "date", formattedDate);
+            }
+        }
+
+        String date = messageContext.getContextData(userId, "date");
+        String city = messageContext.getContextData(userId, "city");
+
+        String verifiedAnswers = verifyAnswers(userId, date, city);
+
+        if(verifiedAnswers.equals("")) {
             return answer;
         }
         return verifiedAnswers;
     }
 
-    private String verifyAnswers(String userId){
-        String userDate = messageContext.getContextData(userId, "date");
-        String userCity = messageContext.getContextData(userId, "city");
+    private String verifyAnswers(String userId, String userDate, String userCity){
 
         if(!userDate.equals("") && !userCity.equals("")){
-            messageContext.deleteContextData(userId, "date");
-            messageContext.deleteContextData(userId, "city");
-            messageContext.deleteContextData(userId, "flag");
+            deleteDataFromUserContext(userId);
             return createAnswer(userDate, userCity);
         }
         return "";
+    }
+
+    private void deleteDataFromUserContext(String userId){
+        messageContext.deleteContextData(userId, "date");
+        messageContext.deleteContextData(userId, "city");
+        messageContext.deleteContextData(userId, "flag");
     }
 
     private String getFormattedDate(String currentDate){
